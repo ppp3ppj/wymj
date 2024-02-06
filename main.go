8b51f0f4628beda2aa0eb69f9a9028d0580a4ffd
ppp <ppp.3ppj@gmail.com>
2024-02-07 00:58:36 +0700

    set config env and print test.

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"os"
+    "fmt"
+	"github.com/ppp3ppj/wymj/config"
+)
 
+func envPath() string {
+    if len(os.Args) == 1 {
+        return ".env"
+    } else {
+        return os.Args[1]
+    }
+}
+
+func main() {
+    cfg := config.LoadConfig(envPath())
+    fmt.Println(cfg.App())
+    fmt.Println(cfg.Db())
+    fmt.Println(cfg.Jwt())
 }
