d83021a97c9154039dbfaf7e5151dbdc90d80c78
ppp <ppp.3ppj@gmail.com>
2024-02-07 23:10:35 +0700

    add http response custom

--- modules/monitor/monitorHandlers/monitor_handler.go
+++ modules/monitor/monitorHandlers/monitor_handler.go
@@ -3,6 +3,7 @@ package monitorHandlers
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/ppp3ppj/wymj/config"
+	"github.com/ppp3ppj/wymj/modules/entities"
 	"github.com/ppp3ppj/wymj/modules/monitor"
 )
 
@@ -25,5 +26,6 @@ func (h *monitorHandler) HealthCheck(c *fiber.Ctx) error {
         Name: h.cfg.App().Name(),
         Version: h.cfg.App().Version(),
     }
-    return c.Status(fiber.StatusOK).JSON(res)
+    //return c.Status(fiber.StatusOK).JSON(res)
+    return entities.NewResponse(c).Success(fiber.StatusOK, res).Res()
 }
